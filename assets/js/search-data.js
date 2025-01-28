7c41b954d5721829a5082503ce4236a4acbb8450
ericdunipace <edunipace@mail.harvard.edu>
2025-01-28 22:35:26 +0000

    Deploying to gh-pages from @ ericdunipace/ericdunipace.github.io@715cdef2f8ecf85690f1ca4b63da0321054cc9d1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -136,16 +136,8 @@ ninja.data = [{
           window.location.href = "/blog/2019/bias-variance/";
         
       },
-    },{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+    },{id: "news-a-new-website-look-is-live-sparkles-smile",
+          title: 'A new website look is live! :sparkles: :smile:',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
